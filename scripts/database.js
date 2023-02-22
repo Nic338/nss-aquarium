6e281520b579bfb8913d9f0806bb54b750825333
Nicholas Myers <nic338@gmail.com>
2023-02-22 15:52:34 -0500

    day 2 of working on the aquarium

---
+++ scripts/database.js
@@ -0,0 +1,73 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            species: "Barticus vertebrata"
+            name: "Bart",
+            size: 3
+            harvested: "Bartstown"
+            food: "crustaceans",
+        }
+        {
+            species: "Derpetris vertebrata"
+            name:"The Pacific Derp Fish"
+            size: .4
+            harvested: "O'ahu"
+            food: "small fish"
+        }
+        {
+            species: "Lachnolmaius maximus"
+            name:"Hogfish"
+            size: 1.4
+            harvested:"Eastern Coast of Mexico"
+            food:"molluscs, crabs, and sea urchins"
+        }
+        {
+            species: "Gadus morhua"
+            name: "Atlantic Cod"
+            size: 1.6
+            harvested: "Maine"
+            food: "crustaceans"
+        }
+        {
+            species: "Lophius piscatorius"
+            name: "Monkfish"
+            size: .45
+            harvested:"North Atlantic Ocean"
+            food:"Fish"
+        }
+        {
+            species: "Epinephelus morio"
+            name: "Red Grouper"
+            size: .78
+            harvested: "Florida Keys"
+            food: "Soft coral and sea sponges"
+        }
+        {
+            species: "Salvelinus foninalis"
+            name: "Brook Trout"
+            size: .24
+            harvested: "West Virginia"
+            food: "aquatic invertibrates"
+        }
+        {
+            species: "Pomacanthus paru"
+            name: "French Angelfish"
+            size: .31
+            harvested: "Gulf of Mexico"
+            food: "Sponges, algae, broyzoans, zoantharians, gorgonians, hydroids, coral, and tunicates"
+        }
+        {
+            species: "Amphiprion ocellaris"
+            name: "Clownfish"
+            size: .1
+            harvested: "Western Coast of Japan"
+            food: "Plankton and algae"
+        }
+    ]
+}
